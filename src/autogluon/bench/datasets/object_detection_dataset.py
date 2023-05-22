985f6ae5024411ea06e703f225d0497dfa44708d
FANGAreNotGnu <kevinfang97@gmail.com>
2023-05-22 23:24:51 +0000

    add agbench

--- src/autogluon/bench/datasets/object_detection_dataset.py
+++ src/autogluon/bench/datasets/object_detection_dataset.py
@@ -2,12 +2,26 @@ import abc
 import os
 
 from autogluon.common.loaders import load_zip
+from autogluon.multimodal.constants import (
+    MAP,
+    MAP_50,
+    MAP_75,
+    MAP_SMALL,
+    MAP_MEDIUM,
+    MAP_LARGE,
+    MAR_1,
+    MAR_10,
+    MAR_100,
+    MAR_SMALL,
+    MAR_MEDIUM,
+    MAR_LARGE,
+)
 
 from .constants import _OBJECT_DETECTION
 from .utils import get_data_home_dir, get_repo_url
 
 # Add dataset class names here
-__all__ = ["TinyMotorbike", "Clipart"]
+__all__ = ["TinyMotorbike", "Clipart", "ApparelBloggerInfluencer", "Comic", "Pothole"]
 
 
 class BaseObjectDetectionDataset(abc.ABC):
@@ -22,9 +36,21 @@ class BaseObjectDetectionDataset(abc.ABC):
         pass
 
     @property
-    @abc.abstractmethod
     def metric(self):
-        pass
+        return [
+            MAP,
+            MAP_50,
+            MAP_75,
+            MAP_SMALL,
+            MAP_MEDIUM,
+            MAP_LARGE,
+            MAR_1,
+            MAR_10,
+            MAR_100,
+            MAR_SMALL,
+            MAR_MEDIUM,
+            MAR_LARGE,
+        ]
 
     @property
     def problem_type(self):
@@ -56,10 +82,6 @@ class TinyMotorbike(BaseObjectDetectionDataset):
     def data(self):
         return self._data_path
 
-    @property
-    def metric(self):
-        return "map"
-
 
 class Clipart(BaseObjectDetectionDataset):
     _SOURCE = "https://github.com/naoto0804/cross-domain-detection/tree/master/datasets"
@@ -86,6 +108,57 @@ class Clipart(BaseObjectDetectionDataset):
     def data(self):
         return self._data_path
 
+
+class AGDetBenchDataset(BaseObjectDetectionDataset):
+    _SOURCE = ""
+    _BENCHMARK_NAME = "AGDetBench"
+
+    def __init__(self, dataset_name, split="train", sha1sum=None):
+        self._dataset_name = dataset_name
+        self._split = split
+        self._sha1sum = sha1sum
+        self._path = os.path.join(get_data_home_dir(), self._dataset_name)
+
+        self._INFO = {
+            "data": {
+                "url": self.data_url,
+                "sha1sum": self._sha1sum,
+            },
+        }
+
+        load_zip.unzip(self._INFO["data"]["url"], unzip_dir=self._path, sha1sum=self._INFO["data"]["sha1sum"])
+        self._base_folder = os.path.join(self._path, self._dataset_name)
+        self._data_path = os.path.join(self._base_folder, "annotations", f"{self._split}.json")
+
     @property
-    def metric(self):
-        return "map"
+    def data_url(self):
+        return get_repo_url() + f"{self._BENCHMARK_NAME}/{self._dataset_name}.zip"
+
+    @property
+    def base_folder(self):
+        return self._base_folder
+
+    @property
+    def data(self):
+        return self._data_path
+
+
+class ApparelBloggerInfluencer(AGDetBenchDataset):
+    _registry_name = "apparel_blogger_influencer"
+
+    def __init__(self, split="train"):
+        super().__init__(dataset_name="apparel_blogger_influencer", split=split, sha1sum=None)
+
+
+class Comic(AGDetBenchDataset):
+    _registry_name = "comic"
+
+    def __init__(self, split="train"):
+        super().__init__(dataset_name="comic", split=split, sha1sum=None)
+
+
+class Pothole(AGDetBenchDataset):
+    _registry_name = "pothole"
+
+    def __init__(self, split="train"):
+        super().__init__(dataset_name="pothole", split=split, sha1sum=None)
