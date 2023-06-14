19a89920fff9057cd4d269690d1e6702b12bae5b
FANGAreNotGnu <kevinfang97@gmail.com>
2023-06-14 20:08:41 +0000

    update detection datasets

--- src/autogluon/bench/datasets/object_detection_dataset.py
+++ src/autogluon/bench/datasets/object_detection_dataset.py
@@ -21,7 +21,40 @@ from .constants import _OBJECT_DETECTION
 from .utils import get_data_home_dir, get_repo_url
 
 # Add dataset class names here
-__all__ = ["TinyMotorbike", "Clipart", "ApparelBloggerInfluencer", "Comic", "Pothole"]
+__all__ = [
+    "TinyMotorbike",
+    "ApparelBloggerInfluencer",
+    "ApparelFashionShow",
+    "ApparelStreetwearDaily",
+    "Chest10",
+    "Cityscapes",
+    "Clipart",
+    "Comic",
+    "DamagedVehicles",
+    "Deepfruits",
+    "Deeplesion",
+    "Dota",
+    "Duo",
+    "Ena24",
+    "F1",
+    "Kitchen",
+    "Kitti",
+    "Lisa",
+    "Mario",
+    "Minneapple",
+    "NflLogo",
+    "Oktoberfest",
+    "Pothole",
+    "Rugrats",
+    "Sixray",
+    "Table",
+    "Tt100k",
+    "Uefa",
+    "Utensils",
+    "VehiclesTestCommercial",
+    "Voc0712",
+    "Widerface",
+]
 
 
 class BaseObjectDetectionDataset(abc.ABC):
@@ -83,41 +116,15 @@ class TinyMotorbike(BaseObjectDetectionDataset):
         return self._data_path
 
 
-class Clipart(BaseObjectDetectionDataset):
-    _SOURCE = "https://github.com/naoto0804/cross-domain-detection/tree/master/datasets"
-    _INFO = {
-        "data": {
-            "url": get_repo_url() + "few_shot_object_detection/clipart.zip",
-            "sha1sum": "d25b2f905da597d7857297ac8e3efe4555e0bf32",
-        },
-    }
-    _registry_name = "clipart"
-
-    def __init__(self, split="train"):
-        self._split = split
-        self._path = os.path.join(get_data_home_dir(), "clipart")
-        load_zip.unzip(self._INFO["data"]["url"], unzip_dir=self._path, sha1sum=self._INFO["data"]["sha1sum"])
-        self._base_folder = os.path.join(self._path, "clipart")
-        self._data_path = os.path.join(self._base_folder, "Annotations", f"{self._split}_cocoformat.json")
-
-    @property
-    def base_folder(self):
-        return self._base_folder
-
-    @property
-    def data(self):
-        return self._data_path
-
-
 class AGDetBenchDataset(BaseObjectDetectionDataset):
     _SOURCE = ""
     _BENCHMARK_NAME = "AGDetBench"
+    _registry_name = None
 
-    def __init__(self, dataset_name, split="train", sha1sum=None):
-        self._dataset_name = dataset_name
+    def __init__(self, split="train", sha1sum=None):
         self._split = split
         self._sha1sum = sha1sum
-        self._path = os.path.join(get_data_home_dir(), self._dataset_name)
+        self._path = os.path.join(get_data_home_dir(), self._registry_name)
 
         self._INFO = {
             "data": {
@@ -127,12 +134,12 @@ class AGDetBenchDataset(BaseObjectDetectionDataset):
         }
 
         load_zip.unzip(self._INFO["data"]["url"], unzip_dir=self._path, sha1sum=self._INFO["data"]["sha1sum"])
-        self._base_folder = os.path.join(self._path, self._dataset_name)
+        self._base_folder = os.path.join(self._path, self._registry_name)
         self._data_path = os.path.join(self._base_folder, "annotations", f"{self._split}.json")
 
     @property
     def data_url(self):
-        return get_repo_url() + f"{self._BENCHMARK_NAME}/{self._dataset_name}.zip"
+        return get_repo_url() + f"{self._BENCHMARK_NAME}/{self._registry_name}.zip"
 
     @property
     def base_folder(self):
@@ -146,19 +153,122 @@ class AGDetBenchDataset(BaseObjectDetectionDataset):
 class ApparelBloggerInfluencer(AGDetBenchDataset):
     _registry_name = "apparel_blogger_influencer"
 
-    def __init__(self, split="train"):
-        super().__init__(dataset_name="apparel_blogger_influencer", split=split, sha1sum=None)
+
+class ApparelFashionShow(AGDetBenchDataset):
+    _registry_name = "apparel_fashion_show"
+
+
+class ApparelStreetwearDaily(AGDetBenchDataset):
+    _registry_name = "apparel_streetwear_daily"
+
+
+class Chest10(AGDetBenchDataset):
+    _registry_name = "chest10"
+
+
+class Cityscapes(AGDetBenchDataset):
+    _registry_name = "cityscapes"
+
+
+class Clipart(AGDetBenchDataset):
+    _registry_name = "clipart"
 
 
 class Comic(AGDetBenchDataset):
     _registry_name = "comic"
 
-    def __init__(self, split="train"):
-        super().__init__(dataset_name="comic", split=split, sha1sum=None)
+
+class DamagedVehicles(AGDetBenchDataset):
+    _registry_name = "damaged_vehicles"
+
+
+class Deepfruits(AGDetBenchDataset):
+    _registry_name = "deepfruits"
+
+
+class Deeplesion(AGDetBenchDataset):
+    _registry_name = "deeplesion"
+
+
+class Dota(AGDetBenchDataset):
+    _registry_name = "dota"
+
+
+class Duo(AGDetBenchDataset):
+    _registry_name = "duo"
+
+
+class Ena24(AGDetBenchDataset):
+    _registry_name = "ena24"
+
+
+class F1(AGDetBenchDataset):
+    _registry_name = "f1"
+
+
+class Kitchen(AGDetBenchDataset):
+    _registry_name = "kitchen"
+
+
+class Kitti(AGDetBenchDataset):
+    _registry_name = "kitti"
+
+
+class Lisa(AGDetBenchDataset):
+    _registry_name = "lisa"
+
+
+class Mario(AGDetBenchDataset):
+    _registry_name = "mario"
+
+
+class Minneapple(AGDetBenchDataset):
+    _registry_name = "minneapple"
+
+
+class NflLogo(AGDetBenchDataset):
+    _registry_name = "nfl_logo"
+
+
+class Oktoberfest(AGDetBenchDataset):
+    _registry_name = "oktoberfest"
 
 
 class Pothole(AGDetBenchDataset):
     _registry_name = "pothole"
 
-    def __init__(self, split="train"):
-        super().__init__(dataset_name="pothole", split=split, sha1sum=None)
+
+class Rugrats(AGDetBenchDataset):
+    _registry_name = "rugrats"
+
+
+class Sixray(AGDetBenchDataset):
+    _registry_name = "sixray"
+
+
+class Table(AGDetBenchDataset):
+    _registry_name = "table"
+
+
+class Tt100k(AGDetBenchDataset):
+    _registry_name = "tt100k"
+
+
+class Uefa(AGDetBenchDataset):
+    _registry_name = "uefa"
+
+
+class Utensils(AGDetBenchDataset):
+    _registry_name = "utensils"
+
+
+class VehiclesTestCommercial(AGDetBenchDataset):
+    _registry_name = "vehicles_test_commercial"
+
+
+class Voc0712(AGDetBenchDataset):
+    _registry_name = "voc0712"
+
+
+class Widerface(AGDetBenchDataset):
+    _registry_name = "widerface"
