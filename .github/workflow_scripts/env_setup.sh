1241259ba4e2165e424d3c52e277b1a050e3671e
Su Zhou <zhousu@amazon.com>
2023-07-13 19:25:13 +0000

    use tox for test and linter

--- .github/workflow_scripts/env_setup.sh
+++
@@ -1,9 +0,0 @@
-function setup_lint_env {
-    python3 -m pip install --upgrade pip
-    python3 -m pip install "black>=22.3,<23.0"
-    python3 -m pip install "isort>=5.10"
-}
-
-function install_bench_test {
-    python3 -m pip install --upgrade -e "./[tests]"
-}
