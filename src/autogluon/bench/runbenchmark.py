2e6b2ac8adf514b5960ef45e11958e1f09b33add
Weisu Yin <weisy@amazon.com>
2023-08-03 18:08:05 +0000

    black

--- src/autogluon/bench/runbenchmark.py
+++ src/autogluon/bench/runbenchmark.py
@@ -329,7 +329,9 @@ def run(
                 instance_type: str = configs["cdk_context"]["INSTANCE"]
                 if not instance_type.startswith(("p", "g")):
                     # CPU instances
-                    os.environ["AG_BENCH_BASE_IMAGE"] = "763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-training:1.13.1-cpu-py39-ubuntu20.04-ec2"
+                    os.environ[
+                        "AG_BENCH_BASE_IMAGE"
+                    ] = "763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-training:1.13.1-cpu-py39-ubuntu20.04-ec2"
                 _validate_single_value(configs["module_configs"]["tabular"], "framework")
                 os.environ["AMLB_FRAMEWORK"] = configs["module_configs"]["tabular"]["framework"][0]
 
