ee055b6ba064b17f29c8063e37b1403b0a2b59ec
Su Zhou <zhousu@amazon.com>
2023-08-07 22:15:25 +0000

    add hardware utilization
    supports multi-gpu utilization
    supports high resolution for custom metrics
    publishes every 5 seconds

--- src/autogluon/bench/entrypoint.sh
+++ src/autogluon/bench/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "Starting Cron server..."
-service cron start
-
+echo "Running hardware utilization monitoring in the background..."
+./hardware_utilization.sh &
 
 if [ -n "$AG_BENCH_DEV_URL" ]; then
     echo "Using Development Branch: $AG_BENCH_DEV_URL" >&2
