a2130591249d4b4db6b6b2c2d6d83b567a09427d
Prateek M Desai <prateekmdesai04@gmail.com>
2024-10-30 16:00:40 -0700

    fix AutoMM by adding uv (#108)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>

--- src/autogluon/bench/frameworks/multimodal/setup.sh
+++ src/autogluon/bench/frameworks/multimodal/setup.sh
@@ -16,26 +16,27 @@ repo_name=$(basename -s .git $(echo $GIT_URI))
 git clone --depth 1 --single-branch --branch ${BRANCH} --recurse-submodules ${GIT_URI} $venv_base_dir/$repo_name
 
 # create virtual env
-python3 -m venv $venv_base_dir/.venv
+python -m venv $venv_base_dir/.venv
 source $venv_base_dir/.venv/bin/activate
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade setuptools wheel
+python -m uv pip install --upgrade pip
+python -m pip install uv
+python -m uv pip install --upgrade setuptools wheel
 
 if echo "$AG_BENCH_VERSION" | grep -q "dev"; then
   # install from local source or docker
-  python3 -m pip install .
+  python -m uv pip install .
 else
-  python3 -m pip install autogluon.bench==$AG_BENCH_VERSION
+  python -m uv pip install autogluon.bench==$AG_BENCH_VERSION
 fi
 
 cd $venv_base_dir/$repo_name
 
-python3 -m pip install -e common
-python3 -m pip install -e core[all]
-python3 -m pip install -e features
-python3 -m pip install -e multimodal
+python -m uv pip install -e common
+python -m uv pip install -e core[all]
+python -m uv pip install -e features
+python -m uv pip install -e multimodal
 
-python3 -m mim install "mmcv==2.1.0"
-python3 -m pip install "mmdet==3.2.0"
+python -m mim install "mmcv==2.1.0"
+python -m uv pip install "mmdet==3.2.0"
 
